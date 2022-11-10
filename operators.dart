7a481668ce92f6398386b2f2d582f52dee23212d
Peter Cheune <peterkayk@gmail.com>
2022-11-10 14:46:26 +0300

    learning Dart class

---
+++ operators.dart
@@ -0,0 +1,9 @@
+void main() {
+  int x = 2;
+  int y = 4;
+  int z = 5;
+
+  z = x + y;
+
+  print("$x + $y is $z");
+}
---
+++ variables.dart
@@ -0,0 +1,7 @@
+void main() {
+  var name = "Peter Kyeyune";
+  var age = 21;
+  var district = "Kampala";
+
+  print("My name is $name, I am $age, I come from $district");
+}
